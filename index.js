270c7ee7b788440727fb16adc9fd5c775d6c5556
Parsha Pourkhomami <parshap+git@gmail.com>
2014-02-13 00:28:14 +0000

    Use built-in Number.isInteger if available

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // https://github.com/paulmillr/es6-shim
 // ES6 isInteger Polyfill https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Number/isInteger#Polyfill
-module.exports = function(val) {
+module.exports = Number.isInteger || function(val) {
 	return typeof val === "number" &&
 		! Number.isNaN(val) &&
 		Number.isFinite(val) &&
