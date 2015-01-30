0042b413f23a86c5aefd8a1d44024b3c4720ad19
Parsha Pourkhomami <parshap+git@gmail.com>
2015-01-30 14:24:10 -0800

    Use Number.isNaN and Number.isFinite ponyfills
    Implementation is now es3-compatible.

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 // https://github.com/paulmillr/es6-shim
 // ES6 isInteger Polyfill https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Number/isInteger#Polyfill
+var isNaN = require("is-nan");
+var isFinite = require("is-finite");
 module.exports = Number.isInteger || function(val) {
 	return typeof val === "number" &&
-		! Number.isNaN(val) &&
-		Number.isFinite(val) &&
+		! isNaN(val) &&
+		isFinite(val) &&
 		val > -9007199254740992 &&
 		val < 9007199254740992 &&
 		parseInt(val, 10) === val;
